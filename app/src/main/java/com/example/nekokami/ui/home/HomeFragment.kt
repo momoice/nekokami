8b66634dc4912de4ecd8e94b744efaa5454f9449
Yousuke Tanaka <yousuke01@live.jp>
2024-09-23 11:06:32 +0900

    ハッカソン後修正6 0:00に記録がリセットされない問題解決！！やっとだよ！！！
    変更情報
    1.ResetTaskCompletedReceiver.ktを削除
    2.HomeFragment.ktで、Alarm系の関数（１のファイルを参照していたもの）全て削除
    正常動作確認済

--- app/src/main/java/com/example/nekokami/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/nekokami/ui/home/HomeFragment.kt
@@ -1,10 +1,6 @@
 package com.example.nekokami.ui.home
 
-import android.app.AlarmManager
-import android.app.PendingIntent
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -21,7 +17,6 @@ import androidx.fragment.app.Fragment
 import com.example.nekokami.R
 import com.example.nekokami.databinding.FragmentHomeBinding
 import java.text.SimpleDateFormat
-import java.util.Calendar
 import java.util.Date
 import java.util.Locale
 
